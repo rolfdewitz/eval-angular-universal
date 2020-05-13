744fcdbc62b2825b809416ab820c92ac537623ae
Rolf Dewitz <rolf@adaapa.com>
2020-05-13 14:41:28 +0200

    Linter

--- src/app/main/services/network.service.ts
+++ src/app/main/services/network.service.ts
@@ -7,19 +7,23 @@ export class NetworkService {
   constructor(private http: HttpClient) {}
 
   getList() {
-    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' });
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    });
     const completeUri = `${this.url}?fields=id,name,href,location`;
     console.warn('Network: GET', completeUri);
-    return this.http.get(completeUri, { headers});
+    return this.http.get(completeUri, { headers });
   }
 
   getDetails(id: string) {
     if (!id) {
       return;
     }
-    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' });
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    });
     const completeUri = `${this.url}/${id}`;
     console.warn('Network: GET', completeUri);
-    return this.http.get(completeUri, { headers});
+    return this.http.get(completeUri, { headers });
   }
 }
